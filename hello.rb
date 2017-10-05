238c5b0e940e19b4bbdaabf2be2ab1e21d0e1f02
yokoyan <yokoyansan@gmail.com>
2017-10-05 03:54:06 +0000

    3章まで完了

--- hello.rb
+++ hello.rb
@@ -1,5 +1,5 @@
+# lesson2
 # コメント
-
 =begin
 コメント
 コメント
@@ -7,6 +7,26 @@
 コメント
 =end
 
-print "hello world"
-puts "hello world" # +改行
-p "hello world" # デバッグ用
+# print "hello world"
+# puts "hello world" # +改行
+# p "hello world" # デバッグ用
+
+# lesson3
+# 変数
+# - 英小文字から始める
+msg = "hello world"
+puts msg
+
+msg = "hello world again"
+puts msg
+
+# 定数
+# - 英大文字
+# 書き換えはできない
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
+
