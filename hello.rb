43a94724ec824dd203c10e466d2f3830b8554379
yokoyan <yokoyansan@gmail.com>
2017-10-08 03:42:29 +0000

    lesson12まで完了

--- hello.rb
+++ hello.rb
@@ -180,3 +180,67 @@ p scores
 p scores.to_a
 # 配列をハッシュに戻す
 p scores.to_a.to_h
+
+# lesson11
+puts "----------------lesson11----------------"
+# %記法
+# 以下はすべて同じ意味
+puts "hello"
+puts %Q(hello) # double Quoteの頭文字のQ
+puts %(hello)
+
+# 以下はすべて同じ意味
+puts 'hello'
+puts %q(hello) # single quoteの頭文字のq
+
+# 文字列の中で区切り文字を使うときに、コードが見やすくなる。=>エスケープが不要になる
+puts %(ダブルクォーテーション)
+puts "he\"llo"
+puts %Q(he"llo)
+puts %(he"llo)
+
+puts %q(シングルクォーテーション)
+puts 'he\'llo'
+puts %q(he'llo)
+
+# 配列の場合
+p ["red", "blue"]
+p %W(red blue) # %Qと同じ
+
+p ['red', 'blue']
+p %w(red blue) # %qと同じ
+
+
+# lesson12
+puts %Q(----------------lesson12----------------)
+# 書式付きで値を埋め込む
+# 文字列 % 値
+p "name: %s" % "taguchi"
+# 10桁確保
+p "name: %10s" % "taguchi"
+# 左に寄せて10桁確保
+p "name: %-10s" % "taguchi"
+
+# 整数と浮動小数点の埋め込み
+# %05d 5桁の整数。足りない場合は先頭を0埋め
+# %10.2f 全体の文字数が10文字、小数点は2桁
+p "id: %05d, rate: %010.2f" % [355, 3.284]
+
+printf("name: %10s\n", "taguchi")
+printf("id: %05d, rate: %10.2f\n", 355, 3.284)
+
+puts("--sprintf--")
+p sprintf("name: %10s\n", "taguchi")
+p sprintf("id: %05d, rate: %10.2f\n", 355, 3.284)
+
+# lesson13
+puts %Q(----------------lesson13----------------)
+
+# lesson14
+puts %Q(----------------lesson14----------------)
+
+# lesson15
+puts %Q(----------------lesson15----------------)
+
+
+
